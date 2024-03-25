7a923ae23eed83ad884da479b97e3e992a7c5e11
jdolinsky <jacobdol@jacobs-air.lan>
2024-03-25 18:13:59 -0500

    initial commit

---
+++ airflow/dags/food_service.py
@@ -0,0 +1,95 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectsWithPrefixExistenceSensor
+from airflow.providers.google.cloud.operators.gcs import GCSDeleteObjectsOperator
+from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.operators.python import PythonOperator
+
+import pendulum
+import pandas as pd
+import logging
+import scripts.cleanup as cl
+import os
+
+
+#set timezone for pendulum
+time_zone = 'America/Chicago'
+
+# GCS Stuff
+LOCATION = "us-central1"
+BUCKET = "food-orders-us"
+NAME_PREFIX = "food_daily"
+
+LOCAL_PATH = "/tmp/food"
+
+default_args = {
+    "owner": "JD",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=3),
+    # google cloud operator/sesor parameters
+    "gcp_conn_id": "google_cloud_food_service",
+    "prefix": "food_daily",
+    "bucket": "food-orders-us",
+    "bucket_name": "food-orders-us",
+    # other parameters
+    "params": {
+        "local_path":"/tmp/food"
+    }
+}
+
+
+# executables
+def list_file_names(bucket, prefix):
+    gcs_hook = GCSHook(gcp_conn_id="google_cloud_food_service")
+    files = gcs_hook.list(bucket_name=bucket,
+                          prefix=prefix)
+    if files :
+        return files[0]
+    else : 
+        return None
+
+
+# [START instantiate_dag]
+with DAG(dag_id="food_service_pipeline",
+         schedule_interval="@daily",
+         start_date=pendulum.datetime(2024, 2, 21, tz=time_zone),
+         default_args=default_args,
+         catchup=False
+         ) as dag:
+# [END instantiate_dag]
+        
+    data_file_available = GCSObjectsWithPrefixExistenceSensor(task_id="wait_for_file",
+                                                   mode='poke',
+                                                   poke_interval = 60,
+                                                   # stop after 4 minutes
+                                                   timeout=240 
+                                                   )
+
+    list_files = PythonOperator(task_id="list_files",
+                                python_callable=list_file_names,
+                                # pass it into executable
+                                op_kwargs={"bucket": "food-orders-us", "prefix": "food_daily"},
+                                do_xcom_push=True
+                                )
+    
+    download_file = GCSToLocalFilesystemOperator(
+        task_id="download_file",
+        object_name="{{task_instance.xcom_pull('list_files', key='return_value')}}",
+        filename=os.path.join("{{params.local_path}}", "{{task_instance.xcom_pull('list_files', key='return_value')}}"),
+    )
+
+    # clean data in downloaded csv
+    clean_data = PythonOperator(task_id="clean_data",
+                                python_callable=cl.data_cleanup,
+                                op_kwargs={"file_path": "{{params.local_path}}", "file_name": "{{task_instance.xcom_pull('list_files', key='return_value')}}"}
+                                )
+    
+    # delete file from google storage once it was downloaded
+    delete_file_from_gcs = GCSDeleteObjectsOperator(task_id="delete_from_GCS",
+                                                    prefix=None,
+                                                    objects=["{{task_instance.xcom_pull('list_files', key='return_value')}}"]
+                                                    )
+    
+    # upload cleaned file to gcs /processed folder
+    data_file_available >> list_files >> download_file >> [delete_file_from_gcs, clean_data]
---
+++ airflow/dags/scripts/cleanup.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import re, os
+
+# Quick cleanup of data
+
+# Remove trailing colons, and special characters from items
+def rm_colon_spchars(text):
+    # define special characters to remove
+    special = "[@_!#$%^&*()<>?/\|}{~]"
+    text = re.sub(special, '', text)
+    # remove trailing colons 
+    if text[-1] == ":" :
+        return text[:-1]
+    else :
+        return text
+
+# clean up
+def data_cleanup(file_path, file_name):
+    # load data
+    fullpath = os.path.join(file_path, file_name)  
+    df = pd.read_csv(fullpath)
+    # Remove duplicate rows
+    df.drop_duplicates(inplace=True)
+    # remove colons and convert to lower case
+    df["items"] = df["items"].map(rm_colon_spchars).map(lambda t : t.lower())
+    # save to csv
+    outdir = 'clean'
+    outdir = os.path.join(file_path, outdir)
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    fullpath = os.path.join(outdir, file_name)   
+    df.to_csv(fullpath)
+
+# test
+if __name__ == '__main__':
+    data_cleanup("mnt/files", "food_daily_2024-03-16.csv")
